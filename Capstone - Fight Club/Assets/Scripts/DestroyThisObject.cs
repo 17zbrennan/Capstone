bc4b43e1eac60a317e4506a8d2a2ae391f052d9d
Zac <zrb13@pct.edu>
2020-09-23 13:53:31 -0400

    Added the new executable.

---
+++ Capstone - Fight Club/Assets/Scripts/DestroyThisObject.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+public class DestroyThisObject : NetworkBehaviour {
+
+    private GameObject parent;
+	// Use this for initialization
+	void Start () {
+        parent = this.transform.parent.gameObject;
+        StartCoroutine("Punch");
+	}
+
+    IEnumerator Punch()
+    {
+        PlayerController p = parent.GetComponent<PlayerController>();
+        p.SetPunch(false);
+        yield return new WaitForSeconds(1.4f);
+        NetworkServer.Destroy(this.gameObject);
+        yield return null;
+
+    }
+}
